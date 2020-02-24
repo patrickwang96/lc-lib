2e9a70d534529e03f660a889a3aeb9c7d9f3d1ff
Patrick WANG <ruochwang@gmail.com>
2020-02-24 17:30:21 +0800

    push 12 interger to roman

---
+++ 012 Integer to Roman.cpp
@@ -0,0 +1,66 @@
+
+// https://leetcode.com/problems/integer-to-roman/
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string result;
+        while (num) {
+            if (num >= 1000){
+                num -= 1000;
+                result += "M";
+            }
+            else if (num >= 900) {
+                num -= 900;
+                result += "CM";
+            }
+            else if (num >= 500) {
+                num -= 500;
+                result += "D";
+            }
+            else if (num >= 400) {
+                num -= 400;
+                result += "CD";
+            }
+            else if (num >= 100) {
+                num -= 100;
+                result += "C";
+            }
+            else if (num >= 90) {
+                num -= 90;
+                result += "XC";
+            }
+            else if (num >= 50) {
+                num -= 50;
+                result += "L";
+            }
+            else if (num >= 40) {
+                num -= 40;
+                result += "XL";
+            }
+            else if (num >= 10) {
+                num -= 10;
+                result += "X";
+            }
+            else if (num >= 9) {
+                num -= 9;
+                result += "IX";
+            }
+            else if (num >= 5) {
+                num -= 5;
+                result += "V";
+            }
+            else if (num >= 4) {
+                num -= 4;
+                result += "IV";
+            }
+            else{
+                num -= 1;
+                result += "I";
+            }
+            
+        }
+        return result;
+    }
+    
+};
