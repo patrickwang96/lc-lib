ba361df4f09f3221a44ad1db959b5de5c6fd78fc
Patrick WANG <ruochwang@gmail.com>
2020-02-27 23:04:45 +0800

    bitwise operation

---
+++ 137 Single Number II.cpp
@@ -0,0 +1,21 @@
+
+// https://leetcode.com/problems/single-number-ii/
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sum;
+        int result = 0;
+        for (int i = 0; i < 32; i++){
+            sum = 0;
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[j] & 1) sum ++;
+                nums[j] = nums[j] >> 1;
+            }
+            sum %= 3;
+            if (sum) {
+                result ^= (1 << i);
+            }
+        }
+        return result;
+    }
+};
---
+++ 371 Sum of Two Integers.cpp
@@ -0,0 +1,16 @@
+
+// https://leetcode.com/problems/sum-of-two-integers/
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        
+        while (b) {
+            int carry = (unsigned int)(a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+            
+    }
+};
